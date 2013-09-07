15e4e81eb6a91f562522290500e16c3716be56d3
MakerSquare Student <students@themakersquare.com>
2013-09-07 15:54:30 -0500

    Add ability to check in books

--- library.rb
+++ library.rb
@@ -1,3 +1,14 @@
+# DONE Users should be able to add books to a library 
+# DONE Books should be able to have information saved about them (author, title, description)
+# A user should be able to check out books from the library for one week intervals
+# DONE A user should not be able to check out more than two books at any given time
+# Checked-out books should be associated with a user
+# Users with overdue books should not be able to request any new books until they turn all their overdue books in
+# DONE Users should be able to check in books to the library when they're finished with them
+# DONE Users should be able to check a book's status (e.g. available, checked out, overdue or lost)
+# Users should be able to see a list of who has checked out which book and when those books are due to be returned
+# Users should be able to see a list of books that are overdue
+
 
 
 class User
@@ -10,10 +21,10 @@ class User
 end
 
 class Book
-  attr_accessor :status
+  attr_accessor :status, :due_date
   attr_reader :title, :author, :description
 
-  def initialize(title, author, description)
+  def initialize(title, author, description=nil)
     @title = title
     @author = author
     @description = description
@@ -24,10 +35,12 @@ end
 class Library
   def initialize
     @books = []
-  end
+  end 
 
   def add_book(book)
-    @books << book
+    if book.is_a?(Book)
+      @books << book
+    end
   end
 
   # Users should be able to check a book's status 
@@ -45,7 +58,7 @@ class Library
   # Returns the array with the new user + book added to the end
 
   def check_out(user, book)
-    if book.status == "available"
+    if book.status == "available" && user.checked_out_books.length < 2 # && OVERDUE = nil
       user.checked_out_books << book
       book.status = "checked out"
       puts "You checked out #{book.title}."
@@ -55,24 +68,32 @@ class Library
   end
 
   def check_in(user, book)
-    # 
-  end
-
-  def overdue_books
-    puts "#{@title}, checked by #{@name}" if @status == "overdue"
+    if user.checked_out_books.include?(book)
+      user.checked_out_books.delete_if { |b| b == book } 
+      book.status  = "available"
+      puts "Thanks for returning #{book.title}!"
+    else
+      puts "You don't have that book to check in."
+    end
   end
 
   def checked_out_books
-    puts "#{@title}" if @status == "checked out"
   end
+
+  def overdue_books(book)
+    puts "#{@title}, checked by #{@name}" if Book.status == "overdue"
+  end
+
 end 
 
 library = Library.new
 amy = User.new("amy")
+kevin = User.new("kevin")
 
-derp = Book.new("derp", "k", "na")
-herp = Book.new("herp", "a", "k")
-hamlet = Book.new("hamlet", "shake", "ya")
+derp = Book.new("derp", "nyan", "cats")
+herp = Book.new("herp", "finn and jake", "royal promises")
+hamlet = Book.new("hamlet", "shakespeare", "everyone dies")
+
 
 puts "Book 1 title: #{derp.title}"
 puts "Book 2 title: #{herp.title}"
@@ -88,4 +109,19 @@ library.check_out(amy, derp)
 
 library.book_status
 
+library.check_out(amy, derp)
+
 library.checked_out_books
+
+library.check_in(amy, derp)
+
+library.check_in(amy, herp)
+
+library.check_out(kevin, derp)
+
+library.checked_out_books
+
+puts "FUCK YEAH"
+
+
+
